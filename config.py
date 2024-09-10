f8214b2781fa92bfb1318b094602698f14529e4d
RAJASAHAB3893 <Virusjones18@gmail.com>
2024-09-10 23:40:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Rajasahab3891")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Rajasahab3891")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Sanatani_Vibes")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Sanatani_Vibes")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -78,13 +78,13 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://i.pinimg.com/originals/e5/22/67/e5226705ea32881348b52f76ac3b5777.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/41a7bfb511fec60c8c6a4.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org/file/354ef51a5f0ce82808018.png"
+    "PING_IMG_URL", "https://telegra.ph/file/4815cb5639784581c99c6.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/4815cb5639784581c99c6.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
